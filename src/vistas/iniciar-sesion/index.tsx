26b3b1ae9080df1f0193c117e37fa2176a2a8419
KarinaRedondo <kari24red@gmail.com>
2024-07-31 22:20:03 -0500

    Se agrego vista registro

---
+++ src/vistas/iniciar-sesion/index.tsx
@@ -0,0 +1,11 @@
+
+
+const IniciarSesion = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default IniciarSesion
---
+++ src/vistas/registrarse/index.tsx
@@ -0,0 +1,26 @@
+import styles from "./Registrarse.module.css";
+
+const Registrarse = () => {
+  return (
+    <div className={styles.contenedor}>
+      <div className={styles.seccion_imagen}>
+        <img src="/registrarse.png" alt="" />
+      </div>
+      <div className={styles.seccion_formulario}>
+        <div className={styles.header}>
+          <h1>Crear cuenta</h1>
+        </div>
+        <div className={styles.contenedor_formulario}>
+          <div className={styles.grupo_input}>
+            <input type="text" placeholder="Ingresa tu nombre" />
+            <input type="text" placeholder="Ingresa tu apellido" />
+          </div>
+          <input type="email" placeholder="Correo electronico" />
+          <input type="password" placeholder="Contraseña" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Registrarse;
