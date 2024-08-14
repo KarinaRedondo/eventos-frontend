e94f08d4bc5218547964c8276e3b9bfaaa473125
matosr96 <19matos96@gmail.com>
2024-08-14 16:49:13 -0500

    update layout

---
+++ src/Componentes/Header/index.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from "react";
+import styles from "./Header.module.css";
+import { User } from "../../models/user";
+
+const Header = () => {
+  const [usuario, setUsuario] = useState<User>({
+    nombre: "EDGAR",
+    apellido: "Matos",
+    id: "1",
+    contraseña: "",
+    correo: "",
+    rol: "",
+  });
+
+  // useEffect(() => {
+  //   // Obtener el objeto usuario desde localStorage y parsearlo
+  //   const storedUsuario = localStorage.getItem("usuario");
+  //   if (storedUsuario) {
+  //     setUsuario(JSON.parse(storedUsuario));
+  //   }
+  // }, []);
+
+  return (
+    <div className={styles.cn_header}>
+      <div className={styles.logo}>
+        {usuario && (
+          <div className={styles.avatar}>
+            {usuario.nombre.charAt(0).toUpperCase()}
+          </div>
+        )}
+      </div>
+      <div className={styles.info_User}>
+        <span>
+          {usuario ? `Bienvenido, ${usuario.nombre}` : "Usuario no disponible"}
+        </span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/Componentes/Sidebar/index.tsx
+++ src/Componentes/Sidebar/index.tsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
-import { Home, Info, Briefcase, Phone } from "react-feather";
+import { Home, Info, Briefcase, Phone, LogOut } from "react-feather";
 import estilos from "./Sidebar.module.css";
 import { Menu, Minimize } from "react-feather";
+import { User } from "../../models/user";
+import { useEffect, useState } from "react";
 
 interface PropsBarraLateral {
   estaAbierto: boolean;
@@ -9,6 +11,22 @@ interface PropsBarraLateral {
 }
 
 const Sidebar = ({ estaAbierto, alternarBarraLateral }: PropsBarraLateral) => {
+  const [usuario, setUsuario] = useState<User>({
+    nombre: "Edgar",
+    apellido: "Matos",
+    id: "1",
+    contraseña: "",
+    correo: "19matos96@gmail.com",
+    rol: "",
+  });
+
+  // useEffect(() => {
+  //   // Obtener el objeto usuario desde localStorage y parsearlo
+  //   const storedUsuario = localStorage.getItem("usuario");
+  //   if (storedUsuario) {
+  //     setUsuario(JSON.parse(storedUsuario));
+  //   }
+  // }, []);
   return (
     <div
       className={`${estilos.barraLateral} ${
@@ -21,46 +39,70 @@ const Sidebar = ({ estaAbierto, alternarBarraLateral }: PropsBarraLateral) => {
         </button>
       </div>
 
-      <div className={estilos.contenedor_enlaces}>
-        <Link to="/" className={estilos.enlace} onClick={alternarBarraLateral}>
-          <span className={estilos.icono}>
-            <Home size={24} />
+      <div className={estilos.cn_header}>
+        <div className={estilos.logo}>
+          {usuario && (
+            <div className={estilos.avatar}>
+              {usuario.nombre.charAt(0).toUpperCase()}
+            </div>
+          )}
+        </div>
+        <div className={estaAbierto ? estilos.info_User : estilos.info_oculta}>
+          <span>
+            {usuario.nombre} {usuario.apellido}
           </span>
-          <span className={estilos.texto}>Home</span>
-        </Link>
+          <span className={estilos.correo}>{usuario.correo}</span>
+        </div>
+      </div>
 
-        <Link
-          to="/about"
-          className={estilos.enlace}
-          onClick={alternarBarraLateral}
-        >
-          <span className={estilos.icono}>
-            <Info size={24} />
-          </span>
-          <span className={estilos.texto}>About</span>
-        </Link>
+      <div className={estilos.contenedor_body}>
+        <div className={estilos.contenedor_enlaces}>
+          <Link
+            to="/"
+            className={estilos.enlace}
+            onClick={alternarBarraLateral}
+          >
+            <span className={estilos.icono}>
+              <Home size={24} />
+            </span>
+            <span className={estilos.texto}>Inicio</span>
+          </Link>
 
-        <Link
-          to="/services"
-          className={estilos.enlace}
-          onClick={alternarBarraLateral}
-        >
-          <span className={estilos.icono}>
-            <Briefcase size={24} />
-          </span>
-          <span className={estilos.texto}>Services</span>
-        </Link>
+          <Link
+            to="/about"
+            className={estilos.enlace}
+            onClick={alternarBarraLateral}
+          >
+            <span className={estilos.icono}>
+              <Info size={24} />
+            </span>
+            <span className={estilos.texto}>Eventos</span>
+          </Link>
 
-        <Link
-          to="/contact"
-          className={estilos.enlace}
-          onClick={alternarBarraLateral}
-        >
-          <span className={estilos.icono}>
-            <Phone size={24} />
-          </span>
-          <span className={estilos.texto}>Contact</span>
-        </Link>
+          <Link
+            to="/services"
+            className={estilos.enlace}
+            onClick={alternarBarraLateral}
+          >
+            <span className={estilos.icono}>
+              <Briefcase size={24} />
+            </span>
+            <span className={estilos.texto}>Asistentes</span>
+          </Link>
+        </div>
+
+        <div className={estilos.cerrar_Sesion}>
+          <Link
+            to="/contact"
+            className={estilos.enlace}
+            onClick={alternarBarraLateral}
+          >
+            <span className={estilos.icono}>
+              <LogOut size={24} />
+            </span>
+            <span className={estilos.texto}>Salir</span>
+          </Link>
+        </div>
       </div>
     </div>
   );
