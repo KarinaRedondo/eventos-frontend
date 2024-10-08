7d54dfcf1d8962defd31ddc2b67d1d7768d8e419
KarinaRedondo <kari24red@gmail.com>
2024-10-08 13:44:59 -0500

    modal

---
+++ src/Componentes/Modal/index.tsx
@@ -0,0 +1,34 @@
+import { ReactNode } from "react";
+import styles from "./Modal.module.css";
+
+interface PropsModal {
+  openModal: boolean;
+  setOpenModal: (value: boolean) => void;
+  nombreModal: string;
+  children: React.ReactNode;
+}
+
+export const ModalComponente = ({
+  openModal,
+  setOpenModal,
+  nombreModal,
+  children,
+}: PropsModal) => {
+  const cerrarModal = () => {
+    setOpenModal(false);
+  };
+
+  if (!openModal) return null;
+
+  return (
+    <div className={styles.contenedorModal}>
+      <div className={styles.modal}>
+        <div className={styles.headerModal}>
+          <h2>{nombreModal}</h2>
+          <button onClick={cerrarModal}>Cerrar</button>
+        </div>
+        <div className={styles.contenido}>{children}</div>
+      </div>
+    </div>
+  );
+};
--- src/vistas/eventos/index.tsx
+++ src/vistas/eventos/index.tsx
@@ -4,8 +4,24 @@ import { obtenerEventos } from "../../servicios/eventos/eventos";
 import { Evento } from "../../models/eventos";
 import styles from "./Eventos.module.css";
 import TarjetaEvento from "../../Componentes/TarjetaEvento";
+import { ModalComponente } from "../../Componentes/Modal";
+import { Search } from "react-feather";
 
 const Eventos = () => {
+  const [nombre, setNombre] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [capacidadMaxima, setCapacidadMaxima] = useState(0);
+  const [fechaInicio, setFechaInicio] = useState("");
+  const [fechaFin, setFechaFin] = useState("");
+  const [ubicacion, setUbicacion] = useState("");
+  const [organizador, setOrganizador] = useState("");
+
+  const [abrirModal, setAbrirModal] = useState(false);
+
+  const abrirModalFuncion = () => {
+    setAbrirModal(true);
+  };
+
   const [eventos, setEventos] = useState<Evento[]>();
 
   const peticionObtenerEventos = async () => {
@@ -25,6 +41,10 @@ const Eventos = () => {
         <h1>Eventos</h1>
         <h4>Dashboard/eventos</h4>
       </div>
+      <div>
+        <input type="Search" placeholder="Buscar eventos" />
+        <button onClick={abrirModalFuncion}>Crear un evento</button>
+      </div>
 
       <div className={styles.contenedor_tarjetas_eventos}>
         {eventos?.map((evento: Evento) => (
@@ -36,6 +56,80 @@ const Eventos = () => {
           />
         ))}
       </div>
+      <ModalComponente
+        openModal={abrirModal}
+        setOpenModal={setAbrirModal}
+        nombreModal="Crear evento"
+      >
+        <form action="">
+          <div className={styles.InputFormulario}>
+            <label>Nombre del evento:</label>
+            <input
+              type="text"
+              name="nombre"
+              placeholder="Nombre del evento"
+              onChange={(e) => setNombre(e.target.value)}
+            />
+          </div>
+
+          <div className={styles.InputFormulario}>
+            <label>Descripción del evento:</label>
+            <textarea
+              name="descripcion"
+              placeholder="Descripción del evento"
+              onChange={(e) => setDescripcion(e.target.value)}
+            />
+          </div>
+
+          <div className={styles.InputFormulario}>
+            <label>Capacidad máxima:</label>
+            <input
+              type="number"
+              name="capacidadMaxima"
+              placeholder="Capacidad máxima"
+              onChange={(e) => setCapacidadMaxima(e.target.valueAsNumber)}
+            />
+          </div>
+
+          <div className={styles.InputFormulario}>
+            <label>Fecha de inicio:</label>
+            <input
+              type="date"
+              name="fechaInicio"
+              onChange={(e) => setFechaInicio(e.target.value)}
+            />
+          </div>
+
+          <div className={styles.InputFormulario}>
+            <label>Fecha de fin:</label>
+            <input
+              type="date"
+              name="fechaFin"
+              onChange={(e) => setFechaFin(e.target.value)}
+            />
+          </div>
+
+          <div className={styles.InputFormulario}>
+            <label>Ubicación:</label>
+            <input
+              type="text"
+              name="ubicacion"
+              placeholder="Ubicación"
+              onChange={(e) => setUbicacion(e.target.value)}
+            />
+          </div>
+
+          <div className={styles.InputFormulario}>
+            <label>Organizador:</label>
+            <input
+              type="text"
+              name="organizador"
+              placeholder="Organizador"
+              onChange={(e) => setOrganizador(e.target.value)}
+            />
+          </div>
+        </form>
+      </ModalComponente>
     </Layout>
   );
 };
